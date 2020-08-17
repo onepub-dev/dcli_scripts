2143c90a61f6c0296f738d0ba7bbff8e33aa04aa
Brett Sutton <bsutton@noojee.com.au>
2020-08-17 15:47:27 +1000

    Added downit which changes the ownership of a path to the current user.

---
+++ downit.dart
@@ -0,0 +1,29 @@
+#! /usr/bin/env dshell
+
+import 'dart:io';
+import 'package:dshell/dshell.dart';
+import 'package:args/args.dart';
+
+/// This script runs chown for the given path(s) making the calling user the owner.
+/// The script uses sudo.
+
+void main(List<String> args) {
+  var parser = ArgParser();
+
+  var results = parser.parse(args);
+  var rest = results.rest;
+
+  if (rest.isEmpty) {
+    printerr('You must provide at least one path');
+    exit(1);
+  }
+
+  var user = Shell.current.loggedInUser;
+
+  for (var path in rest.toList()) {
+    if (!exists(path)) {
+      print(red("The path ${truepath(path)} doesn't exists. Skipped"));
+    }
+    'chown -R $user:$user $path'.start(privileged: true);
+  }
+}
