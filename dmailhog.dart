ab3f4a1a8a873916597453e3dafecd9ff3510e62
Brett Sutton <bsutton@noojee.com.au>
2020-08-20 16:22:24 +1000

    built script to install and launch mail hog.

---
+++ dmailhog.dart
@@ -0,0 +1,40 @@
+#! /usr/bin/env dshell
+
+import 'package:dshell/dshell.dart';
+
+/// installs (if necessary) and runs mailhog
+
+void main() {
+  install();
+
+  run();
+}
+
+void run() {
+  print(green('Starting mailhog'));
+
+  print(orange('Access mail hog at: http://localhost:8025'));
+  mailHogAppPath.run;
+}
+
+void install() {
+  if (!exists(mailHogDirectoryPath)) {
+    createDir(mailHogDirectoryPath, recursive: true);
+  }
+
+  if (!exists(mailHogAppPath)) {
+    print(orange('Installing mailhog'));
+    'wget https://github.com/mailhog/MailHog/releases/download/v1.0.0/MailHog_linux_amd64'
+        .start(workingDirectory: mailHogDirectoryPath);
+    'cp MailHog_linux_amd64 $mailHogAppPath'.start(privileged: true, workingDirectory: mailHogDirectoryPath);
+    'chmod +x $mailHogAppPath'.start(privileged: true);
+  }
+}
+
+String get mailHogDirectoryPath {
+  return join(HOME, 'apps', 'mailhog');
+}
+
+String get mailHogAppPath => join(mailHogDirectoryPath, mailHogAppname);
+
+String get mailHogAppname => 'mailhog';
