02cee43c384d77a091ed81f3033aaadf2c1bfb3f
Brett Sutton <bsutton@noojee.com.au>
2022-04-20 08:39:19 +1000

    Improved the help for the dmysql restore command.

--- lib/src/mysql/restore_command.dart
+++ lib/src/mysql/restore_command.dart
@@ -6,14 +6,17 @@ import 'mysql_settings.dart';
 
 class RestoreCommand extends Command<void> {
   @override
-  String get description => 'restores up a database.';
+  String get description => '''
+restores a database.
+dmysql restore <db name> [path to backup]''';
 
   @override
   String get name => 'restore';
 
   @override
   void run() {
-    final args = getArgs(this, argResults, additionalArgs: ['backup file.sql']);
+    final args =
+        getArgs(this, argResults, additionalArgs: ['<path to backup file>']);
     final dbname = args[0];
     final sqlFile = args[1];
 
@@ -21,6 +24,7 @@ class RestoreCommand extends Command<void> {
   }
 
   void restore(MySqlSettings settings, String sqlFile) {
+    print('restoring $sqlFile to ${settings.dbname}');
     'mysql --host ${settings.host} --port=${settings.port} '
             '--user ${settings.user} --password="${settings.password}" '
             '--database ${settings.dbname} '
